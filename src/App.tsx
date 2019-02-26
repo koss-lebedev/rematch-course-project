31be769590a419b79aa9fd08e656779f6eee751a
Constantine Lebedev <koss.lebedev@gmail.com>
2019-02-26 13:18:57 +0100

    Configure rematch store

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React, { Fragment } from 'react'
+import { Provider } from 'react-redux'
 import GlobalStyles from './globalStyles'
 import UserList from './components/UserList'
 import Header from './components/Header'
 import ToastList from './components/ToastList'
+import store from './store'
 
 const App = () => (
-  <Fragment>
-    <GlobalStyles />
-    <ToastList toasts={[]} />
-    <Header count={0} />
-    <UserList
-      users={[]}
-      toggleFollow={_user => {}}
-    />
-  </Fragment>
+  <Provider store={store}>
+    <Fragment>
+      <GlobalStyles />
+      <ToastList toasts={[]} />
+      <Header count={0} />
+      <UserList
+        users={[]}
+        toggleFollow={_user => {}}
+      />
+    </Fragment>
+  </Provider>
 )
 
 export default App
---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { init } from '@rematch/core'
+
+const models = {
+
+}
+
+const store = init({
+  models,
+})
+
+export default store
